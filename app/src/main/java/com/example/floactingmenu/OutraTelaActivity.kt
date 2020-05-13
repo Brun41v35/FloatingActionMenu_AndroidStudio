ce0f3732561b464285d1e45353eab3ab99303e38
Bruno <alves.bruno945@gmail.com>
2020-05-13 16:15:43 -0300

    Using floating menu button

---
+++ app/src/main/java/com/example/floactingmenu/OutraTelaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.floactingmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutraTelaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outra_tela)
+    }
+}
